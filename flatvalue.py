e9be5228a445636b74c85f4e39bc6df3c6f478d0
Rafal Czajkowski <rafalxczajkowski@hotmail.com>
2022-03-31 13:23:46 +0200

    code formatting

--- flatvalue.py
+++ flatvalue.py
@@ -2,14 +2,18 @@ import pandas as pd
 import plotly.express as px
 
 df_his = pd.read_csv('data/historia-kredytu-2008.csv', encoding='Windows-1250')
-df_chf = pd.read_csv('data/CHF.csv', encoding='Windows-1250', sep=';', parse_dates=['Data'])
+df_chf = pd.read_csv('data/CHF.csv', encoding='Windows-1250',
+                     sep=';', parse_dates=['Data'])
 
-df_his['#Data księgowania'] = pd.to_datetime(df_his['#Data księgowania'], format="%d-%m-%Y")
-df_his_short = df_his.loc[df_his['#Opis'].str.contains('SPŁATA'), ['#Data księgowania', '#Kwota', '#Kwota pozostała do spłaty']]
+df_his['#Data księgowania'] = pd.to_datetime(
+    df_his['#Data księgowania'], format="%d-%m-%Y")
+df_his_short = df_his.loc[df_his['#Opis'].str.contains(
+    'SPŁATA'), ['#Data księgowania', '#Kwota', '#Kwota pozostała do spłaty']]
 df_his_short.columns = ['Date', 'CHF amount', 'CHF left to pay']
 
 df_chf_short = df_chf.loc[:, ['Data', 'CHF (Cena sprzedazy)']]
 df_chf_short.columns = ['Date', 'CHF rate']
+
 # Remove time part from Timestamp
 df_chf_short.loc[:, 'Date'] = df_chf_short['Date'].dt.normalize()
 # Merge history of payments df and CHF rates df
@@ -25,9 +29,9 @@ df_his_chf.loc[df_his_chf['PLN left to pay'] == 0, ['PLN left to pay']] = None
 df_his_chf_na = df_his_chf.dropna(how="any")
 # create plot
 fig = px.line(df_his_chf_na, x="Date", y=['Sum of payments in PLN', 'PLN left to pay'],
-title='Paying off CHF mortgage',
-width=1000,
-height=600)
+              title='Paying off CHF mortgage',
+              width=1000,
+              height=600)
 # create html code containing the plot
 def create_html():
     return fig.to_html("index.html", include_plotlyjs='cdn', full_html=True)
